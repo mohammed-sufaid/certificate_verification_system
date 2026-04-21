1266108c1b3c80aa544803c7227a0583ce18c9d9
Sufaid M <71865993+mohammed-sufaid@users.noreply.github.com>
2026-04-21 19:31:45 +0530

    Delete Backend directory

--- Backend/CertificateVerification.Application/Interfaces/IJwtService.cs
+++
@@ -1,8 +0,0 @@
-using CertificateVerification.Domain.Entities;
-
-namespace CertificateVerification.Application.Interfaces;
-
-public interface IJwtService
-{
-    string GenerateToken(User user, string roleName);
-}
--- Backend/CertificateVerification.Domain/Entities/Role.cs
+++
@@ -1,7 +0,0 @@
-namespace CertificateVerification.Domain.Entities;
-
-public class Role
-{
-    public int Id { get; set; }
-    public string RoleName { get; set; } = string.Empty;
-}
